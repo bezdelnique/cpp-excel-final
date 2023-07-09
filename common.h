496d34e24d133fccd1a1f1aa56d3cc817c4e691d
Vitaly Belenky <heman777@yandex.ru>
2023-07-09 13:07:55 +0300

    Formula wrapper

--- common.h
+++ common.h
@@ -27,12 +27,15 @@ struct Position {
 
 // Описывает ошибки, которые могут возникнуть при вычислении формулы.
 class FormulaError : public std::runtime_error {
-public:
-    using std::runtime_error::runtime_error;
+ public:
+  using std::runtime_error::runtime_error;
 };
 
 // Исключение, выбрасываемое при попытке задать синтаксически некорректную
 // формулу
 class FormulaException : public std::runtime_error {
-public:
-    using std::runtime_error::runtime_error;};
+ public:
+  using std::runtime_error::runtime_error;
+};
+
+std::ostream &operator<<(std::ostream &output, FormulaError fe);
