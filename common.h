10d4780ffc51624d04700e4e003c18598e7da27a
Vitaly Belenky <heman777@yandex.ru>
2023-07-09 12:13:48 +0300

    Formula wrapper

--- common.h
+++ common.h
@@ -1,38 +1,18 @@
 #pragma once
 
 #include <stdexcept>
-#include <sstream>
-#include <string>
-#include <string_view>
-
-// Позиция ячейки. Индексация с нуля.
-struct Position {
-  int row = 0;
-  int col = 0;
-
-  bool operator==(Position rhs) const;
-  bool operator<(Position rhs) const;
-
-  bool IsValid() const;
-  std::string ToString() const;
-
-  static Position FromString(std::string_view str);
-
-  static const int MAX_ROWS = 16384;
-  static const int MAX_COLS = 16384;
-  static const int MAX_ROWS_ZB = MAX_ROWS - 1;
-  static const int MAX_COLS_ZB = MAX_COLS - 1;
-  static const Position NONE;
-};
 
 // Описывает ошибки, которые могут возникнуть при вычислении формулы.
 class FormulaError : public std::runtime_error {
-public:
-    using std::runtime_error::runtime_error;
+ public:
+  using std::runtime_error::runtime_error;
 };
+
+std::ostream& operator<<(std::ostream& output, FormulaError fe);
 
 // Исключение, выбрасываемое при попытке задать синтаксически некорректную
 // формулу
 class FormulaException : public std::runtime_error {
-public:
-    using std::runtime_error::runtime_error;};
+ public:
+  using std::runtime_error::runtime_error;
+};
--- structures.cpp
+++ structures.cpp
@@ -4,12 +4,12 @@
 #include <sstream>
 #include <cmath>
 
-const int LETTERS = 26;
-const int MAX_POSITION_LENGTH = 17;
-const int MAX_POS_DIGIT_COUNT = 8;
-const int MAX_POS_LETTER_COUNT = 3;
-
-const Position Position::NONE = {-1, -1};
+//const int LETTERS = 26;
+//const int MAX_POSITION_LENGTH = 17;
+//const int MAX_POS_DIGIT_COUNT = 8;
+//const int MAX_POS_LETTER_COUNT = 3;
+//
+//const Position Position::NONE = {-1, -1};
 
 //int IntPow(int base, int exp) {
 //  return static_cast<int>(std::pow(base, exp));
@@ -24,108 +24,108 @@ const Position Position::NONE = {-1, -1};
 //  return exp;
 //}
 
-// Реализуйте методы:
-bool Position::operator==(const Position rhs) const {
-  return col == rhs.col && row == rhs.row;
-}
-
-bool Position::operator<(const Position rhs) const {
-  if (col == rhs.col) {
-    return row < rhs.row;
-  }
-  return col < rhs.col;
-}
-
-bool Position::IsValid() const {
-  if (row < 0 || col < 0 || row > MAX_ROWS_ZB || col > MAX_COLS_ZB) {
-    return false;
-  }
-  return true;
-}
-
-std::string Position::ToString() const {
-  if (col > MAX_COLS_ZB || row > MAX_ROWS_ZB || col < 0 || row < 0) {
-    return "";
-  }
-
-  // Определить разрядность
-  // ZZ - (26*26) + 26
-  static int zz = (LETTERS * LETTERS) + LETTERS;
-
-  std::stringstream ss;
-  // Чтобы математика складывалась
-  //int col = col + 1;
-  if (col > (zz - 1)) {
-    int first = col / (LETTERS * LETTERS);
-    int remains = col - ((LETTERS * LETTERS) * first);
-    int second = remains / LETTERS;
-    int third = remains - (LETTERS * second);
-    ss << static_cast<char>('A' + first - 1)
-       << static_cast<char>('A' + second - 1)
-       << static_cast<char>('A' + third);
-  } else if (col > (LETTERS - 1)) {
-    size_t first = col / LETTERS;
-    size_t second = col % LETTERS;
-    ss << static_cast<char>('A' + first - 1)
-       << static_cast<char>('A' + second);
-  } else {
-    ss << static_cast<char>('A' + col);
-  }
-
-  ss << row + 1;
-  return ss.str();
-}
-
-Position Position::FromString(std::string_view str) {
-  // MAX: XFD16384
-  if (str.empty() || str.size() < 2 || str.size() > (MAX_POS_DIGIT_COUNT + MAX_POS_LETTER_COUNT)) {
-    return Position::NONE;
-  }
-
-  bool digit = false;
-  std::string col_str;
-  std::string row_str;
-  for (const char &ch : str) {
-    if (!std::isdigit(ch) && !std::isupper(ch)) {
-      return Position::NONE;
-    }
-
-    if (std::isdigit(ch)) {
-      digit = true;
-      row_str.push_back(ch);
-    } else {
-      col_str.push_back(ch);
-    }
-
-    if (digit && !std::isdigit(ch)) {
-      return Position::NONE;
-    }
-  }
-
-  if (col_str.size() > MAX_POS_LETTER_COUNT || row_str.size() > MAX_POS_DIGIT_COUNT) {
-    return Position::NONE;
-  }
-  int row = std::stoi(row_str) - 1;
-
-  if (row > MAX_ROWS_ZB) {
-    return Position::NONE;
-  }
-
-  int col = 0;
-  if (col_str.size() == 1) {
-    col = col_str[0] - 'A';
-  } else if (col_str.size() == 2) {
-    col = (LETTERS * (col_str[0] - 'A' + 1)) + col_str[1] - 'A';
-  } else {
-    // MAX: XFD
-    col = ((LETTERS * LETTERS) * (col_str[0] - 'A' + 1))
-        + (LETTERS * (col_str[1] - 'A' + 1))
-        + col_str[2] - 'A';
-  }
-
-  if (col > MAX_COLS_ZB) {
-    return Position::NONE;
-  }
-
-  return {row, col};
-}
+//// Реализуйте методы:
+//bool Position::operator==(const Position rhs) const {
+//  return col == rhs.col && row == rhs.row;
+//}
+//
+//bool Position::operator<(const Position rhs) const {
+//  if (col == rhs.col) {
+//    return row < rhs.row;
+//  }
+//  return col < rhs.col;
+//}
+//
+//bool Position::IsValid() const {
+//  if (row < 0 || col < 0 || row > MAX_ROWS_ZB || col > MAX_COLS_ZB) {
+//    return false;
+//  }
+//  return true;
+//}
+//
+//std::string Position::ToString() const {
+//  if (col > MAX_COLS_ZB || row > MAX_ROWS_ZB || col < 0 || row < 0) {
+//    return "";
+//  }
+//
+//  // Определить разрядность
+//  // ZZ - (26*26) + 26
+//  static int zz = (LETTERS * LETTERS) + LETTERS;
+//
+//  std::stringstream ss;
+//  // Чтобы математика складывалась
+//  //int col = col + 1;
+//  if (col > (zz - 1)) {
+//    int first = col / (LETTERS * LETTERS);
+//    int remains = col - ((LETTERS * LETTERS) * first);
+//    int second = remains / LETTERS;
+//    int third = remains - (LETTERS * second);
+//    ss << static_cast<char>('A' + first - 1)
+//       << static_cast<char>('A' + second - 1)
+//       << static_cast<char>('A' + third);
+//  } else if (col > (LETTERS - 1)) {
+//    size_t first = col / LETTERS;
+//    size_t second = col % LETTERS;
+//    ss << static_cast<char>('A' + first - 1)
+//       << static_cast<char>('A' + second);
+//  } else {
+//    ss << static_cast<char>('A' + col);
+//  }
+//
+//  ss << row + 1;
+//  return ss.str();
+//}
+//
+//Position Position::FromString(std::string_view str) {
+//  // MAX: XFD16384
+//  if (str.empty() || str.size() < 2 || str.size() > (MAX_POS_DIGIT_COUNT + MAX_POS_LETTER_COUNT)) {
+//    return Position::NONE;
+//  }
+//
+//  bool digit = false;
+//  std::string col_str;
+//  std::string row_str;
+//  for (const char &ch : str) {
+//    if (!std::isdigit(ch) && !std::isupper(ch)) {
+//      return Position::NONE;
+//    }
+//
+//    if (std::isdigit(ch)) {
+//      digit = true;
+//      row_str.push_back(ch);
+//    } else {
+//      col_str.push_back(ch);
+//    }
+//
+//    if (digit && !std::isdigit(ch)) {
+//      return Position::NONE;
+//    }
+//  }
+//
+//  if (col_str.size() > MAX_POS_LETTER_COUNT || row_str.size() > MAX_POS_DIGIT_COUNT) {
+//    return Position::NONE;
+//  }
+//  int row = std::stoi(row_str) - 1;
+//
+//  if (row > MAX_ROWS_ZB) {
+//    return Position::NONE;
+//  }
+//
+//  int col = 0;
+//  if (col_str.size() == 1) {
+//    col = col_str[0] - 'A';
+//  } else if (col_str.size() == 2) {
+//    col = (LETTERS * (col_str[0] - 'A' + 1)) + col_str[1] - 'A';
+//  } else {
+//    // MAX: XFD
+//    col = ((LETTERS * LETTERS) * (col_str[0] - 'A' + 1))
+//        + (LETTERS * (col_str[1] - 'A' + 1))
+//        + col_str[2] - 'A';
+//  }
+//
+//  if (col > MAX_COLS_ZB) {
+//    return Position::NONE;
+//  }
+//
+//  return {row, col};
+//}
